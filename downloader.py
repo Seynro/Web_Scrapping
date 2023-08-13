8ae4edcda137a750f2268a93ad3ab0b90d98684f
Seynro <oseymur0@gmail.com>
2023-08-13 16:31:37 +0400

    fixed ads bug

--- downloader.py
+++ downloader.py
@@ -3,8 +3,26 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 from bs4 import BeautifulSoup
 import time 
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 start = time.time()
+
+# def close_ad_if_present():
+#     # Поиск всех элементов с указанным селектором
+#     close_ad_buttons = driver.find_elements(By.CSS_SELECTOR, "div.close-btn")
+
+#     # Если список не пуст, элемент найден, и мы можем кликнуть по нему
+#     if close_ad_buttons:
+#         # Используем JavaScript для выполнения клика
+#         driver.execute_script("arguments[0].click();", close_ad_buttons[0])
+#         print('Closed Ads')
+#     else:
+#         print('No Ads')
+
+
+
 
 def price_taker():
     # Чтение файла
@@ -27,8 +45,6 @@ def price_taker():
             price = price_tag.text.strip()
             prices.append(price)
 
-    print(prices)
-
     return prices
 
 
@@ -38,7 +54,17 @@ df = pd.DataFrame()
 
 makes = ['Audi', 'BMW', 'Toyota', 'Mercedes']
 # Запустите веб-драйвер (например, Chrome)
-driver = webdriver.Chrome()
+from selenium import webdriver
+
+# Путь к файлу расширения
+path_to_adblockplus = r"C:\Users\user\Desktop\extension_3_18_1_0.crx"
+
+# Настройка опций Chrome
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_extension(path_to_adblockplus)
+
+# Запуск Chrome с расширением
+driver = webdriver.Chrome(options=chrome_options)
 
 # Откройте главную страницу
 driver.get("https://ru.turbo.az/")
@@ -56,15 +82,19 @@ for i in makes:
 
     show_results = driver.find_element(By.XPATH, f'//*[@id="new_q"]/div/div[4]/div[2]/button')
     show_results.click()
-
+    pages = 0
     while True:
+        pages += 1
         # Сохраните HTML текущей страницы
         page_html = driver.page_source
 
         with open('page.html', 'w', encoding='utf-8') as file:
             file.write(page_html)
+        
         total.extend(price_taker())
 
+        # time.sleep(2)
+        # close_ad_if_present()
         try:
             # Поиск ссылки на следующую страницу
             next_link = driver.find_element(By.CSS_SELECTOR, 'a[rel="next"]')
@@ -72,9 +102,14 @@ for i in makes:
             # Если ссылка найдена, обновляем URL для следующей итерации
             # Иначе завершаем цикл
             next_link.click()
+            print('PAGE DONE, GOING TO THE NEXT')
+            print(pages)
         except:
+            print('BREAK')
+            print(pages)
             break
-    
+            
+    print('MAKE DONE')
     temp_df = pd.DataFrame({i: total})
     df = pd.concat([df, temp_df], axis=1)
     print(df)
